2703ce8bcb24db50607e09c13808ef9dd039535c
John Gietzen <otac0n@gietzen.us>
2011-08-06 23:38:44 -0500

    Added the samples project.

---
+++ nunit.extensions.samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿// ****************************************************************
+// Copyright © 2011, John Gietzen
+// This is free software licensed under the zlib/libpng license.
+// You may obtain a copy of the license at http://opensource.org.
+// ****************************************************************
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nunit.extensions.samples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("nunit.extensions")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2011\r\nAll rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da31d02e-28ec-4dfd-bc80-851a3aa5274f")]
+
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
