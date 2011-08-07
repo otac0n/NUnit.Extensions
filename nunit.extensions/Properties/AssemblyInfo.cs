73099c7967c71f347961af5ff07bf7dc6d1b8099
John Gietzen <otac0n@gietzen.us>
2011-08-06 23:14:27 -0500

    Added solution and empty project.

---
+++ nunit.extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿// ****************************************************************
+// Copyright © 2011, John Gietzen
+// This is free software licensed under the zlib/libpng license.
+// You may obtain a copy of the license at http://opensource.org.
+// ****************************************************************
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nunit.extensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("nunit.extensions")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2011\r\nAll rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("552f9774-45fe-4622-bd0d-e361c82a9f6d")]
+
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
